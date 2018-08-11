2d1d01eaf3571a6f849722ced7c34271d8575df1
Pierre Chalamet <pierre@chalamet.net>
2018-08-11 21:53:31 +0200

    update sample

--- SandboxFS/Sample.fsx
+++ SandboxFS/Sample.fsx
@@ -5,12 +5,14 @@ open NTerraform.Providers
 open NTerraform.Resources
 
 let provider = azurerm(environment = "dev")
+
 let networkrg = azurerm_resource_group(name = "production",
                                        location = "West US")
 
 let networkSubnets = [| azurerm_virtual_network.subnet(name = "subnet1", addressPrefix = "10.0.1.0/24")
                         azurerm_virtual_network.subnet(name = "subnet2", addressPrefix = "10.0.2.0/24")
                         azurerm_virtual_network.subnet(name = "subnet3", addressPrefix = "10.0.3.0/24") |]
+                        
 let network = azurerm_virtual_network(name = "production-network",
                                       addressSpace = [| "10.0.0.0/16" |],
                                       location = networkrg.Location,
